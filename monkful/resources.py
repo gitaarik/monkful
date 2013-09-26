495c6fe3ee13841ce2e807165b407e6862439c45
Rik <gitaarik@gmail.com>
2013-09-26 16:25:50 +0200

    removed unused import

--- monkful/resources.py
+++ monkful/resources.py
@@ -3,7 +3,6 @@ from flask import request
 from flask.ext.restful import Resource, abort
 from mongoengine import fields
 from mongoengine.errors import NotUniqueError, DoesNotExist, ValidationError
-from .serializers import fields as serializer_fields
 from .serializers.exceptions import (
     UnknownField, ValueInvalidType, DataInvalidType, DeserializeReadonlyField
 )
